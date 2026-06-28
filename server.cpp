bd5f8027140f7d8f3ac945831a5de5fd51382c95
dtgG1856 <metrog@163.com>
2026-06-28 17:16:07 +0800

    feat: 添加了+,-,*,mod 的小数运算，增加 gcd 与 xor 整数限制, 增加 sin 函数计算

--- server.cpp
+++ server.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <iomanip>
 #include "httplib.h"
+
+std::string limit_double(double num){
+    std::ostringstream oss;
+    oss << std::fixed << std::setprecision(6) << num;
+    return oss.str();
+}
+
+bool is_int(std::string str){
+    double a = std::stod(str);
+    long long c = std::stoll(str);
+    if((double)c == a) return true;
+    else return false;
+}
 
 int main() {
     httplib::Server svr;
@@ -16,10 +32,10 @@ int main() {
     // 加法
     svr.Get("/api/add", [](const httplib::Request& req, httplib::Response& res) {
         try {
-            long long a = std::stoll(req.get_param_value("a"));
-            long long b = std::stoll(req.get_param_value("b"));
-            long long result = a + b;
-            res.set_content("{\"result\":" + std::to_string(result) + "}", "application/json");
+            double a = std::stod(req.get_param_value("a"));
+            double b = std::stod(req.get_param_value("b"));
+            double result = a + b;
+            res.set_content("{\"result\":" + limit_double(result) + "}", "application/json");
         } catch (...) {
             res.status = 400;
             res.set_content("{\"error\":\"Invalid input\"}", "application/json");
@@ -29,10 +45,10 @@ int main() {
     // 减法
     svr.Get("/api/sub", [](const httplib::Request& req, httplib::Response& res) {
         try {
-            long long a = std::stoll(req.get_param_value("a"));
-            long long b = std::stoll(req.get_param_value("b"));
-            long long result = a - b;
-            res.set_content("{\"result\":" + std::to_string(result) + "}", "application/json");
+            double a = std::stod(req.get_param_value("a"));
+            double b = std::stod(req.get_param_value("b"));
+            double result = a - b;
+            res.set_content("{\"result\":" + limit_double(result) + "}", "application/json");
         } catch (...) {
             res.status = 400;
             res.set_content("{\"error\":\"Invalid input\"}", "application/json");
@@ -42,10 +58,10 @@ int main() {
     // 乘法
     svr.Get("/api/mul", [](const httplib::Request& req, httplib::Response& res) {
         try {
-            long long a = std::stoll(req.get_param_value("a"));
-            long long b = std::stoll(req.get_param_value("b"));
-            long long result = a * b;
-            res.set_content("{\"result\":" + std::to_string(result) + "}", "application/json");
+            double a = std::stod(req.get_param_value("a"));
+            double b = std::stod(req.get_param_value("b"));
+            double result = a * b;
+            res.set_content("{\"result\":" + limit_double(result) + "}", "application/json");
         } catch (...) {
             res.status = 400;
             res.set_content("{\"error\":\"Invalid input\"}", "application/json");
@@ -55,15 +71,15 @@ int main() {
     // 取模
     svr.Get("/api/mod", [](const httplib::Request& req, httplib::Response& res) {
         try {
-            long long a = std::stoll(req.get_param_value("a"));
-            long long b = std::stoll(req.get_param_value("b"));
+            double a = std::stod(req.get_param_value("a"));
+            double b = std::stod(req.get_param_value("b"));
             if (b == 0) {
                 res.status = 400;
-                res.set_content("{\"error\":\"除数不能为 0\"}", "application/json");
+                res.set_content("{\"error\":\"Invalid input\"}", "application/json");
                 return;
             }
-            long long result = a % b;
-            res.set_content("{\"result\":" + std::to_string(result) + "}", "application/json");
+            double result = std::fmod(a, b);
+            res.set_content("{\"result\":" + limit_double(result) + "}", "application/json");
         } catch (...) {
             res.status = 400;
             res.set_content("{\"error\":\"Invalid input\"}", "application/json");
@@ -73,8 +89,15 @@ int main() {
     // 异或
     svr.Get("/api/xor", [](const httplib::Request& req, httplib::Response& res) {
         try {
-            long long a = std::stoll(req.get_param_value("a"));
-            long long b = std::stoll(req.get_param_value("b"));
+            std::string str_a = req.get_param_value("a");
+            std::string str_b = req.get_param_value("b");
+            if(!is_int(str_a) || !is_int(str_b)){
+                res.status = 400;
+                res.set_content("{\"error\":\"Invalid input\"}", "application/json");
+                return;
+            }
+            long long a = std::stoll(str_a);
+            long long b = std::stoll(str_b);
             long long result = a ^ b;
             res.set_content("{\"result\":" + std::to_string(result) + "}", "application/json");
         } catch (...) {
@@ -86,15 +109,34 @@ int main() {
     // 最大公因数
     svr.Get("/api/gcd", [](const httplib::Request& req, httplib::Response& res) {
         try {
-            long long a = std::stoll(req.get_param_value("a"));
-            long long b = std::stoll(req.get_param_value("b"));
+            std::string str_a = req.get_param_value("a");
+            std::string str_b = req.get_param_value("b");
+            if(!is_int(str_a) || !is_int(str_b)){
+                res.status = 400;
+                res.set_content("{\"error\":\"Invalid input\"}", "application/json");
+                return;
+            }
+            long long a = std::stoll(str_a);
+            long long b = std::stoll(str_b);
             if (a == 0 && b == 0) {
                 res.status = 400;
-                res.set_content("{\"error\":\"输入不合法\"}", "application/json");
+                res.set_content("{\"error\":\"Invalid input\"}", "application/json");
                 return;
             }
             long long result = std::gcd(a,b);
             res.set_content("{\"result\":" + std::to_string(result) + "}", "application/json");
+        } catch (...) {
+            res.status = 400;
+            res.set_content("{\"error\":\"Invalid input\"}", "application/json");
+        }
+    });
+
+    //正弦函数
+    svr.Get("/api/sin", [](const httplib::Request& req, httplib::Response& res) {
+        try {
+            double a = std::stod(req.get_param_value("a"));
+            double result = std::sin(a);
+            res.set_content("{\"result\":" + limit_double(result) + "}", "application/json");
         } catch (...) {
             res.status = 400;
             res.set_content("{\"error\":\"Invalid input\"}", "application/json");
